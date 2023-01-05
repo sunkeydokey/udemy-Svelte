5e9c07aedfd859af658dfafc5e49dbd0fbe7f659
sunkeydokey <sunkeydokey@gmail.com>
2023-01-05 17:36:29 +0900

    section 6: 모달, 컴포넌트 간 통신, 라이프사이클

--- section-6-component/src/App.svelte
+++ section-6-component/src/App.svelte
@@ -1,11 +1,80 @@
 <script>
-	export let name;
+  import { tick, afterUpdate } from 'svelte';
+  import Product from './Product.svelte';
+  import Modal from './Modal.svelte';
+
+  let products = [
+    {
+      id: 'p1',
+      title: 'A Book',
+      price: 20,
+    },
+  ];
+
+  let text = 'This is some Dummy Text!!';
+
+  let showModal;
+  let closable;
+
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(event) {
+    console.log(event);
+  }
+
+  function transform(event) {
+    if (event.which !== 9) {
+      return;
+    }
+    event.preventDefault();
+
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+    const value = event.target.value;
+
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+{#each products as product (product.id)}
+  <!-- <Product
+    title={product.title}
+    price={product.price}
+    on:add-to-cart={addToCart}
+    on:delete={deleteProduct}
+  /> -->
+  <Product {...product} on:add-to-cart={addToCart} on:delete={deleteProduct} />
+{/each}
 
-<h1>Hello {name}!</h1>
+<button on:click={() => (showModal = true)}>Show Modal</button>
+
+{#if showModal}
+  <Modal
+    on:cancel={() => (showModal = false)}
+    on:close={() => (showModal = false)}
+    let:didAgree={closable}
+  >
+    <h1 slot="header">Hello!</h1>
+    <p>Paragraph</p>
+    <button
+      slot="footer"
+      on:click={() => (showModal = false)}
+      disabled={!closable}>Confirm</button
+    >
+  </Modal>
+{/if}
+
+<textarea rows="5" value={text} on:keydown={transform} />
+
+<style>
+</style>
---
+++ section-6-component/src/Modal.svelte
@@ -0,0 +1,88 @@
+<script>
+  import {
+    createEventDispatcher,
+    onMount,
+    onDestroy,
+    beforeUpdate,
+    afterUpdate,
+  } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let agreed = false;
+  let autoscroll = false;
+
+  onMount(() => {
+    console.log('On Mount');
+  });
+
+  onDestroy(() => {
+    console.log('On Destroy');
+  });
+
+  beforeUpdate(() => {
+    console.log('Before Update');
+    autoscroll = agreed;
+  });
+
+  afterUpdate(() => {
+    console.log('After Update');
+    if (autoscroll) {
+      const modal = document.querySelector('.modal');
+      modal.scrollTo(0, modal.scrollHeight);
+    }
+  });
+
+  console.log('Script executed');
+</script>
+
+<div class="backdrop" on:click={() => dispatch('cancel')} />
+<div class="modal">
+  <header>
+    <slot name="header" />
+  </header>
+  <div class="content">
+    <slot />
+  </div>
+  <div class="disclaimer">
+    <p>Before you close, you need to agree to our terms!</p>
+    <button on:click={() => (agreed = true)}>Agree</button>
+  </div>
+  <footer>
+    <slot name="footer" didAgree={agreed}>
+      <button on:click={() => dispatch('close')} disabled={!agreed}
+        >Close</button
+      >
+    </slot>
+  </footer>
+</div>
+
+<style>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.75);
+    z-index: 10;
+  }
+
+  .modal {
+    padding: 1rem;
+    position: fixed;
+    top: 10vh;
+    left: 10%;
+    width: 80%;
+    max-height: 15vh;
+    background: white;
+    border-radius: 5px;
+    z-index: 100;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    overflow: scroll;
+  }
+
+  header {
+    border-bottom: 1px solid #ccc;
+  }
+</style>
